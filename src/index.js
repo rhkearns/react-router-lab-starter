4e889b70851b212492f49379ad6509c9005d33b8
rhkearns <ryankearns217@gmail.com>
2021-12-07 13:27:50 -0500

    initial setup

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './components/App';
 
 import './styles.css';
 
 const rootElement = document.getElementById('root');
-ReactDOM.render(<App />, rootElement);
+ReactDOM.render(
+<App />, rootElement);
